7aec2be4b0c6762451bbafa876f6daf14e3dea37
Unknown <marcosdecris@hotmail.com>
2018-02-02 09:49:44 -0300

    Rutinas Backstack ok (Resolver ViewPager)

--- app/src/main/java/com/example/marcos/appejercicios/View/Rutinas/ContenedorRutinas.java
+++ app/src/main/java/com/example/marcos/appejercicios/View/Rutinas/ContenedorRutinas.java
@@ -18,23 +18,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ContenedorRutinas extends AppCompatActivity implements AdaptadorRecycRutinas.ComunicadorRutinas, FragmentDetalleRutina.EmpezarRtna{
-
+    //Tags
+    private static final String CLAVE_VIEWPAGER_RUTINAS = "Viewpager rutinas";
+    private static final String CLAVE_DETALLE_RUTINA = "Detalle rutina";
+    private static final String CLAVE_EJERCICIO_RUTINA = "Ejercicio rutina";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contenedor_rutinas);
 
         FragmentViewPagerRutina fragmentViewPagerRutina = new FragmentViewPagerRutina();
-        cargarFragment(fragmentViewPagerRutina);
+        cargarFragment(fragmentViewPagerRutina, CLAVE_VIEWPAGER_RUTINAS);
 
     }
 
     //Hago el metodo para cargar fragments
-    public void cargarFragment (Fragment fragment){
+    public void cargarFragment (Fragment fragment, String tag){
         FragmentManager manager = getSupportFragmentManager();
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.replace(R.id.contenedorRutinas, fragment);
-        transaction.commit();
+        Fragment fragmentContenedor = manager.findFragmentById(R.id.contenedorRutinas);
+        if(fragmentContenedor == null || fragmentContenedor.getTag() != tag){
+            FragmentTransaction transaction = manager.beginTransaction();
+            transaction.replace(R.id.contenedorRutinas, fragment);
+            if(fragmentContenedor != null){
+                transaction.addToBackStack(null);
+            }
+            transaction.commit();
+        }
+
+
     }
     //Metodo para ir al detalle de la rutina
     @Override
@@ -45,7 +56,7 @@ public class ContenedorRutinas extends AppCompatActivity implements AdaptadorRec
         bundle.putIntegerArrayList(fragmentDetalleRutina.CLAVE_DETALLE_RUTINA, (ArrayList<Integer>) listaEjercicios);
         bundle.putIntegerArrayList(fragmentDetalleRutina.CLAVE_DETALLE_CALENTAMIENTO, (ArrayList<Integer>) listaEjsEsti);
         fragmentDetalleRutina.setArguments(bundle);
-        cargarFragment(fragmentDetalleRutina);
+        cargarFragment(fragmentDetalleRutina, CLAVE_DETALLE_RUTINA);
     }
 
     @Override
@@ -56,7 +67,7 @@ public class ContenedorRutinas extends AppCompatActivity implements AdaptadorRec
         bundle.putIntegerArrayList(fragmentEjercicioRutina.CLAVE_DETALLE_RUTINA, (ArrayList<Integer>) listaIdsEjsRtna);
         bundle.putIntegerArrayList(fragmentEjercicioRutina.CLAVE_DETALLE_CALENTAMIENTO, (ArrayList<Integer>) listaIdsEjsEsti);
         fragmentEjercicioRutina.setArguments(bundle);
-        cargarFragment(fragmentEjercicioRutina);
+        cargarFragment(fragmentEjercicioRutina, CLAVE_EJERCICIO_RUTINA);
     }
 
 
